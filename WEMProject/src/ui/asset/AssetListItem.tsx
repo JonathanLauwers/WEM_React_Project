c0e5311d079aa6e89e9a56c00352d0825688a53a
Jonathan Lauwers <jonathanlauwers@Jonathans-MacBook-Pro.local>
2019-12-31 15:01:14 +0100

    react-native-elements material design

--- WEMProject/src/ui/asset/AssetListItem.tsx
+++ WEMProject/src/ui/asset/AssetListItem.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Button, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { styles } from './AssetListItem.styles';
 import { Subtitle, H1 } from '../TextHeaders';
 import { Asset } from '../../data/asset/asset';
 import ListItemTransition from '../../animations/ListItemTransition';
 import { CircledImage } from '../CircledImage';
+import { Button } from 'react-native-elements';
 
 type AssetPreview = {
   asset: Asset;
@@ -28,7 +29,7 @@ export const AssetListItem: React.FunctionComponent<AssetPreview> = (asset): JSX
         </TouchableOpacity>
         <View style={styles.buttons}>
           <View style={styles.button}>
-            <Button style={styles.button} color="green" title="Change picture" onPress={() => {asset.navigateCamera(asset.asset.id)}} /> 
+            <Button type="outline" title="Change picture" onPress={() => {asset.navigateCamera(asset.asset.id)}} /> 
           </View>
         </View>
 
